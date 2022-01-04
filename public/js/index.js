d660bcbce83685c4d924a4b90eb65860c8d06334
Muhammad Kamel Umar <72482538+ka-media@users.noreply.github.com>
2022-01-04 10:33:45 +0100

    Add files via upload

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,4 @@
 
- function reset() {
-   document.getElementById('barrelLitres').remove();
-}
-
 function item() {
      const item = prompt('Enter name of item');
      document.getElementById('item').textContent = item;
@@ -15,11 +11,26 @@ function amount() {
      const amount = prompt('Enter amount');
      document.getElementById('amount').textContent = amount;
 }
+function person() {
+     const name = prompt('Enter your name');
+     document.getElementById('name').textContent = name;
+}
 function reset() {
      document.getElementById('default').remove();
     }
 function hide() {
      document.querySelector('.wrap').remove();
+}
+
+
+function randomNum() {
+     const randomNumber = Math.floor(Math.random() * 789);
+     //return Math.floor(Math.random() * 0123456789);
+
+        if (randomNumber !== 0) {
+          id = "DGMC/";
+          document.getElementById('transact').innerHTML = id + randomNumber * 0123456;
+         }
 }
 //Date setting
 let date = new Date() 
